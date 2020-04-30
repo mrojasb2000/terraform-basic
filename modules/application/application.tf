497d2d6bb890554a9477054fa64107b3b630fb3b
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-04-30 19:27:15 -0400

    Add modules and variables.

---
+++ modules/application/application.tf
@@ -0,0 +1,41 @@
+variable "vpc_id" {
+
+}
+variable "subnet_id" {
+  
+}
+variable "name" {
+
+}
+
+
+resource "aws_security_group" "allow_http" {
+  name = "${var.name} allow_http"
+  description = "Allow HTTP traffic"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "mighty-trousers" {
+  ami = "ami-0777ff5c030fe1d38"
+  instance_type = "t3.micro"
+  subnet_id = "${var.subnet_id}"
+  vpc_security_group_ids = ["${aws_security_group.allow_http.id}"]
+  tags = {
+      Name = "${var.name}"
+  }
+}
--- template.tf
+++ template.tf
@@ -17,38 +17,13 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_security_group" "allow_http" {
-  name = "allow_http"
-  description = "Allow HTTP traffic"
+module "mighty-trousers" {
+  source = "./modules/application"
   vpc_id = "${aws_vpc.my_vpc.id}"
-
-  ingress {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_instance" "mightty-trousers" {
-  ami = "ami-0777ff5c030fe1d38"
-  instance_type = "t3.micro"
   subnet_id = "${aws_subnet.public.id}"
-  vpc_security_group_ids = [
-      aws_security_group.allow_http,
-  ]
-  tags = {
-      Name = "master-instance"
-  }
+  name = "MightyTrousers"
 }
+
 
 /*
 resource "aws_instance" "master-instance" {
