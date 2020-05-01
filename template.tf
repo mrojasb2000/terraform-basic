8877b09ea0a1eeee2c401d498b464208ce9e2733
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-04-30 20:48:07 -0400

    Add two modules.

--- template.tf
+++ template.tf
@@ -24,31 +24,9 @@ module "mighty-trousers" {
   name = "MightyTrousers"
 }
 
-
-/*
-resource "aws_instance" "master-instance" {
-  ami = "ami-0777ff5c030fe1d38"
-  instance_type = "t3.micro"
-  subnet_id = "${aws_subnet.public.id}"
-  tags = {
-      Name = "master-instance"
-  }
+module "crazy-foods" {
+    source = "./modules/application"
+    vpc_id = "${aws_vpc.my_vpc.id}"
+    subnet_id = "${aws_subnet.public.id}"
+    name = "CrazyFoods"
 }
-
-resource "aws_instance" "slave-instance" {
-  ami = "ami-0777ff5c030fe1d38"
-  instance_type = "t3.micro"
-  subnet_id = "${aws_subnet.public.id}"
-  depends_on = [
-    aws_instance.master-instance,
-  ]
-  tags = {
-      master_hostname = "${aws_instance.master-instance.private_dns}"
-      Name = "slave-instance"
-  }
-  lifecycle {
-    ignore_changes = [
-        tags
-    ]
-  }
-}*/
